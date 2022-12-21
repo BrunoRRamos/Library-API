8d17e032fe4b0f1e97a4de0b172a74154ba64cee
MisterBeamCs <103748132+BrunoRRamos@users.noreply.github.com>
2022-12-21 20:34:43 -0300

    fixed

--- Server.js
+++ Server.js
@@ -15,7 +15,7 @@ const routes = {
 const server = http.createServer((req, res) => {
     res.writeHead(200, {"Content-type": "text/plain"});
     res.end(routes[req.url]);
-})
+});
 
 //Inicia o servidor
 server.listen(port, () => {
