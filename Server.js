7f32b8c50c091d545fcffef63ba64d213c9eee0b
MisterBeamCs <103748132+BrunoRRamos@users.noreply.github.com>
2022-12-21 20:26:06 -0300

    installing libs Nodemon

--- Server.js
+++ Server.js
@@ -1,11 +1,23 @@
+//Importa a Lib http
 const http = require("http")
+
+//Cria uma porta para o Server ouvir
 const port = 3000;
 
+//Cria as possiveis rotas do Server
+const routes = {
+    '/': 'Batata',
+    '/books': 'Sucess',
+    '/authors': 'Sucess - Author'
+}
+
+//Cria o servidor com req (requisição) e res (resposta) de acordo com a url
 const server = http.createServer((req, res) => {
     res.writeHead(200, {"Content-type": "text/plain"});
-    res.end("Batata");
+    res.end(routes[req.url]);
 })
 
+//Inicia o servidor
 server.listen(port, () => {
     console.log(`Server listening: http://localhost:${port}`)
 });
