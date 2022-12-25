fd52239efdc88ada8132a78e3e6ab069d33ad594
MisterBeamCs <103748132+BrunoRRamos@users.noreply.github.com>
2022-12-24 22:48:04 -0300

    PUT, GET_BY_ID, DELETE

--- src/controllers/booksController.js
+++ src/controllers/booksController.js
@@ -9,11 +9,35 @@ class BookController {
         });
     }
 
+    //Implementa o GET pelo ID
+    static getById = (req, res) => {
+        const id = req.params.id;
+        books.findById(id, (err, books) => {
+            err ? res.status(400).send({message: `${err.message} - Book Not Found - Invalid ID`}) : res.status(200).send(books);
+        });
+    }
+
     //Implementa POST de um livro
     static createBook = (req, res) => {
         let book = new books(req.body)
         book.save((err) => {
-            err ? res.status(500).send(`Message: ${err.message} - POST error`) : res.status(201).send(`POST Sucess`)
+            err ? res.status(500).send(`Message: ${err.message} - POST error`) : res.status(201).send(`Sucess POST`)
+        });
+    }
+
+    //Implementa PUT de um livro
+    static updateBook = (req, res) => {
+        const id = req.params.id;
+        books.findByIdAndUpdate(id, {$set: req.body}, (err) => {
+            !err ? res.status(200).send('Sucess PUT') : res.status(500).send({message: `${err.message} - PUT error`})
+        });
+    }
+
+    //Implementa DELETE de um livro
+    static deleteBook = (req, res) => {
+        const id = req.params.id;
+        books.findByIdAndDelete(id, (err) => {
+            err ? res.status(500).send(`Message: ${err.message} - DELETE error`) : res.status(200).send(`Sucess DELETE`)
         });
     }
 }
--- src/routes/booksRoutes.js
+++ src/routes/booksRoutes.js
@@ -5,9 +5,13 @@ import BookController from "../controllers/booksController.js";
 const router = express.Router();
 
 //Cria GET para a rota /books
-//Cria POST para a rota/books
+//Cria POST para a rota /books
+//Cria PUT para a rota /books/:id
 router
     .get("/books", BookController.listBooks)
+    .get("/books/:id", BookController.getById)
     .post("/books", BookController.createBook)
+    .put("/books/:id", BookController.updateBook)
+    .delete("/books/:id", BookController.deleteBook)
 
 export default router;
