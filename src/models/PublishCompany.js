6db920240aa5ae23e7b1622a0b6f411b71ddae9e
Bruno Rodrigues Ramos <brbosshd2@gmail.com>
2023-01-06 23:40:49 -0300

    Update PublishCompany.js

--- src/models/PublishCompany.js
+++ src/models/PublishCompany.js
@@ -4,7 +4,7 @@ const publishCompanyShema = new mongoose.Schema(
     {
     id: {type: String},
     name: {type: String, required: true},
-    nacionality: {type: String}
+    nacionality: {type: String, required: true}
     },
     {
     versionKey: false
