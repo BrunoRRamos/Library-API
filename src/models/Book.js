5e276e5b73fe10034c25c8f7116a1f588d012914
MisterBeamCs <103748132+BrunoRRamos@users.noreply.github.com>
2023-01-17 11:30:37 -0300

    Update Book.js

--- src/models/Book.js
+++ src/models/Book.js
@@ -4,7 +4,7 @@ import mongoose from "mongoose";
 const bookShema = new mongoose.Schema({
     id: {type: String},
     title: {type: String, required: true},
-    author: {type: String, required: true},
+    author: {type: mongoose.Schema.Types.ObjectId, ref: "Authors", required: true},
     publishCompany: {type: String, required: true},
     numPages: {type: Number}
 });
